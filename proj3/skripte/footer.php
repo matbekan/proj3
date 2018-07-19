2b4742a2ca56d264aad66a7543873397eddd132c
matbekan <40799637+matbekan@users.noreply.github.com>
2018-07-19 14:24:27 +0200

    Add files via upload

---
+++ proj3/skripte/footer.php
@@ -0,0 +1,8 @@
+<html>
+
+	<footer>
+		 <p> Matea, Copyright &copy; 2018 </p>
+		</footer>
+	
+	</body>
+<html>
---
+++ proj3/skripte/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width">
+        <title>en Provence</title>
+        <meta name="author" content="Matea Bekan">
+        <meta name="description" content="prva">
+        <link href="./css/css.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+		<header>
+		<div class="navi">
+				<div id="brand">
+					<h1> <em>L'Occitane en Provence</em></h1>
+				</div>
+			<nav>
+				<ul>
+					<li><a href="index.html">Home </a> </li>
+					<li><a href="vrste.php">Internet trgovina </a> </li>
+					<li><a href="Events.html">Events </a> </li>
+				
+				</ul>
+				</nav>
+				</div>
+		</header>
+		
